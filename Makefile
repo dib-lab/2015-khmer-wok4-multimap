abe7c424035517b928f2998cc64385da6f1d4077
C. Titus Brown <titus@idyll.org>
2015-03-28 16:18:27 -0400

    woiks

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ NULLGRAPH=../nullgraph
 
 all: genomes.fa reads-a.fa reads-b.fa
 
-do:
+clean:
+	-rm -f *.fa
+
+do: reads-a.fa reads-b.fa
 	multi-align.py genomes.fa reads-a.fa
 	multi-align.py genomes.fa reads-b.fa
 
@@ -11,8 +14,8 @@ genomes.fa:
 	$(NULLGRAPH)/make-random-genome.py -l 1000 -s 2 --name='genomeB' > genome-b.fa
 	cat genome-a.fa genome-b.fa > genomes.fa
 
-reads-a.fa: genome-a.fa
-	$(NULLGRAPH)/make-reads.py -r 100 -e 0.00 -C 10 -S 1 genome-a.fa | head -6 > reads-a.fa
+reads-a.fa: genomes.fa
+	$(NULLGRAPH)/make-reads.py -r 100 -C 10 -S 1 genome-a.fa | head -6 > reads-a.fa
 
-reads-b.fa: genome-b.fa
-	$(NULLGRAPH)/make-reads.py -r 100 -e 0.00 -C 10 -S 1 genome-b.fa | head -6 > reads-b.fa
+reads-b.fa: genomes.fa
+	$(NULLGRAPH)/make-reads.py -r 100 -C 10 -S 1 genome-b.fa | head -6 > reads-b.fa
--- multi-align.py
+++ multi-align.py
@@ -9,22 +9,32 @@ def main():
     parser.add_argument('reads')
     args = parser.parse_args()
 
+    # build a countgraph to align to.
     cg = khmer.new_counting_hash(21, 1e7, 4)
     cg.consume_fasta(args.genomes)
+    aligner = khmer.ReadAligner(cg, 1, 1.0)
 
     names = []
     for grec in screed.open(args.genomes):
         names.append(grec.name)
-    
+
+    print 'loaded two references:', names
+
+    # generate a separate LabelHash that (once we have aligned) will
+    # give us the identities of the sequences to which we align.
     lh = khmer.LabelHash(21, 1e7, 4)
     lh.consume_fasta_and_tag_with_labels(args.genomes)
 
-    print lh.n_labels()
-
-    aligner = khmer.ReadAligner(cg, 1, 1.0)
+    # run through all the reads, align to cg, take alignments, and look
+    # up the labels.
     for record in screed.open(args.reads):
+        # build alignments against cg
         _, ga, ra, truncated = aligner.align(record.sequence)
+
+        # now grab the associated labels
         labels = lh.sweep_label_neighborhood(ga)
+
+        # print out the matches.
         matches = set([ names[i] for i in labels ])
         print ", ".join(matches)
 
